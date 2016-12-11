d7bd63d0fc13f85bc100baf9562ec318101ac686
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2016-12-11 22:22:17 +0530

    add validations to account modal

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,18 @@
 class Account < ApplicationRecord
 
-  belongs_to    :team
+  belongs_to                :team
+
+  validates_presence_of     :team
+  validates_presence_of     :name
+  validates_presence_of     :source_id
+  validates_presence_of     :image
+  validates_presence_of     :source
+  validates_presence_of     :type
 
   enum type: {
     profile: 1,
-    page: 2
+    page: 2,
+    community: 3,
   }
 
   enum source: {
